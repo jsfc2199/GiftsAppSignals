b0f9304ff7a2449097bafdca1cbb2ac6cd022cdc
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-25 22:00:41 -0500

    rutas hijas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,16 +3,23 @@ import { Routes } from '@angular/router';
 export const routes: Routes = [
   {
     path: 'dashboard',
-    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component').then(c => c.DashboardPageComponent)
+    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component').then(c => c.DashboardPageComponent),
+    children: [
+      {
+        path: 'trending',
+        loadComponent: () => import('./gifts/pages/trending-page/trending-page.component').then(c => c.TrendingPageComponent)
+      },
+      {
+        path: 'search',
+        loadComponent: () => import('./gifts/pages/search-page/search-page.component').then(c => c.SearchPageComponent)
+      },
+      {
+        path: '**',
+        redirectTo: 'trending'
+      }
+    ]
   },
-  {
-    path: 'trending',
-    loadComponent: () => import('./gifts/pages/trending-page/trending-page.component').then(c => c.TrendingPageComponent)
-  },
-  {
-    path: 'search',
-    loadComponent: () => import('./gifts/pages/search-page/search-page.component').then(c => c.SearchPageComponent)
-  },
+
   {
     path: '**',
     redirectTo: 'dashboard'
