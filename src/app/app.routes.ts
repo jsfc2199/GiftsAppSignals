c2d40998dcca9491708dcdd8feb04ed6e032713e
Juan Sebastian Franco Cervera <jsfc2199@gmail.com>
2025-03-25 21:53:22 -0500

    componentes iniciales

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'dashboard',
+    loadComponent: () => import('./gifts/pages/dashboard-page/dashboard-page.component').then(c => c.DashboardPageComponent)
+  },
+  {
+    path: 'trending',
+    loadComponent: () => import('./gifts/pages/trending-page/trending-page.component').then(c => c.TrendingPageComponent)
+  },
+  {
+    path: 'search',
+    loadComponent: () => import('./gifts/pages/search-page/search-page.component').then(c => c.SearchPageComponent)
+  },
+  {
+    path: '**',
+    redirectTo: 'dashboard'
+  }
+];
